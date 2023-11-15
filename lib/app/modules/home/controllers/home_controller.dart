c695c0dd1f2b6ce0a4833b12d9bb6588eadd8919
michael <michael@cadot.eu>
2023-11-15 13:23:38 +0100

    add 3 seconds for see initialized and private variable locationprovider

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,11 +2,13 @@ import 'package:get/get.dart';
 import 'package:weather/app/data/providers/location_provider.dart';
 
 class HomeController extends GetxController {
-  Rx<String> myposition = RxString("");
-  final locationProvider = Get.put(LocationProvider());
+  Rx<String> myposition = RxString('not initialized');
+  final _locationProvider = Get.put(LocationProvider());
   @override
   Future<void> onInit() async {
     super.onInit();
+    //on met une attente de 2 secondes pour voir le chargement
+    await Future.delayed(const Duration(seconds: 3));
     await getPosition();
   }
 
@@ -17,7 +19,7 @@ class HomeController extends GetxController {
           '{"status":"success","country":"France","countryCode":"FR","region":"IDF","regionName":"Île-de-France","city":"Paris","zip":"75001","lat":48.8323,"lon":2.4075,"timezone":"Europe/Paris","isp":"SCALEWAY","org":"ONLINE","as":"AS12876 SCALEWAY S.A.S.","query":"51.159.148.219"}';
     } else {
       print('get position');
-      var position = await locationProvider.getLocation();
+      var position = await _locationProvider.getLocation();
       if (position != null) {
         print(position.toJson());
         myposition.value = position.toJson().toString();
